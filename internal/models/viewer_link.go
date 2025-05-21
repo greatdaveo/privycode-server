a6ad1c8a36eba4862ae0e1893f6db26de32cc66f
David Olowomeye <olowomeyedavid242@gmail.com>
2025-05-21 18:53:30 +0100

    Updated the Viewer Link model to use snake case

--- internal/models/viewer_link.go
+++ internal/models/viewer_link.go
@@ -8,11 +8,11 @@ import (
 
 type ViewerLink struct {
 	gorm.Model
-	RepoName  string `gorm:"not null"`
-	UserID    uint   `gorm:"not null"`
-	User      User   `gorm:"constraint:OnDelete:CASCADE"`
-	Token     string `gorm:"not null;unique"`
-	ExpiresAt time.Time
-	MaxViews  int
-	ViewCount int
+	RepoName  string    `gorm:"not null" json:"repo_name"`
+	UserID    uint      `gorm:"not null" json:"user_id"`
+	User      User      `gorm:"constraint:OnDelete:CASCADE"`
+	Token     string    `gorm:"not null;unique" json:"token"`
+	ExpiresAt time.Time `json:"expires_at"`
+	MaxViews  int       `json:"max_views"`
+	ViewCount int       `json:"view_count"`
 }
