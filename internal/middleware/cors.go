52f5d67c56352d6c2e6f53fcdd2dbcad09ee4029
David Olowomeye <olowomeyedavid242@gmail.com>
2025-05-23 11:55:34 +0100

    Updated cors for deployment

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -4,20 +4,23 @@ import "net/http"
 
 func WithCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// To allow local frontend
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		origin := r.Header.Get("Origin")
+		allowedOrigins := map[string]bool{
+			"http://localhost:5173":     true,
+			"https://privycode.com":     true,
+			"https://www.privycode.com": true,
+		}
+
+		if allowedOrigins[origin] {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+		}
+
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
-		// To handle preflight (OPTIONS) requests
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
-			return
-		}
-
-		if r.Method == http.MethodOptions {
-			w.WriteHeader(http.StatusNoContent)
 			return
 		}
 
